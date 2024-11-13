26d37dc4793247984615d3a83bc38fccbfe5b6e9
BBackpack <ashleytran.128@gmail.com>
2024-11-13 15:50:59 -0600

    more fixing

--- backend/index.cjs
+++ backend/index.cjs
@@ -33,7 +33,7 @@ const express = require('express');
 const session = require('express-session');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
